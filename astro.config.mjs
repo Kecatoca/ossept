05387870606fc03b39d2d8ec6e951fed2f71218d
Kecatoca <kecatocamix@hotmal.com>
2024-12-02 00:17:18 -0500

    configuracion

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kecatoca.github.io",
-  base: "ossept",
+ 
   integrations: [
     tailwind(),
     icon(),
